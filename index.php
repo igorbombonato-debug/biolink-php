dac8b2234d45bee3687e40d7e8e3f793d6bee7e4
igor Bombonato <igor.bombonato@aluno.senai.br>
2025-11-27 11:55:48 -0300

    atividade renan

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
     <?php
     // --- DADOS DO PERFIL (PREENCHA COM SEUS DADOS) ---
     $nome = "Charlies Brown jr♫♫";
-    $bio = "A vida me ensinou  nunca desistir<br>
-    @Santos<br>
-    𝕾𝖐𝖆𝖙𝖊 𝕭𝖔𝖆𝖗𝖉<br>";
+    $bio = "❝𝖀𝖓𝖘 𝖛𝖎𝖛𝖛𝖊𝖗 𝖈𝖔𝖒𝖔 𝖘𝖊 𝖓𝖚𝖓𝖈𝖆 𝖋𝖔𝖘𝖘𝖊𝖒 𝖒𝖔𝖗𝖗𝖊𝖗 𝖔𝖚𝖙𝖗𝖔𝖘 𝖒𝖔𝖗𝖗𝖊𝖒 𝖈𝖔𝖒𝖔 𝖘𝖊 𝖓𝖚𝖓𝖈𝖆 𝖙𝖎𝖛𝖊𝖘𝖘𝖊𝖒 𝖛𝖎𝖛𝖎𝖉𝖔❞<br><br>
+    @𝕾𝖆𝖓𝖙𝖔𝖘<br><br>
+    𝕾𝖐𝖆𝖙𝖊 𝕭𝖔𝖆𝖗𝖉";
     $imagem ="https://i.pinimg.com/736x/ee/f3/55/eef3556161bfa4884de825e6c16a0248.jpg"; // Pegue o link da SUA foto.
     $links = [
-        "Instagram" => "https://instagram.com/iguinxit",
-        "WhatsApp"  => "https://wa.me/17981924356",
-        "Meu Jogo Favorito" => "https://play.google.com/store/apps/details?id=com.dts.freefireth&hl=pt_BR",
-        "Música do Momento" => "https://www.youtube.com/watch?v=oD9izvb3gO0"
+        "𝐢𝐧𝐬𝐭𝐚𝐠𝐫𝐚𝐦" => "https://instagram.com/iguinxit",
+        "𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩"  => "https://wa.me/17981924356",
+        "𝐌𝐞𝐮 𝐣𝐨𝐠𝐨 𝐟𝐚𝐯𝐨𝐫𝐢𝐭𝐨" => "https://play.google.com/store/apps/details?id=com.dts.freefireth&hl=pt_BR",
+        "𝐌𝐮𝐬𝐢𝐜𝐚 𝐝𝐨 𝐌𝐨𝐦𝐞𝐧𝐭𝐨" => "https://www.youtube.com/watch?v=oD9izvb3gO0"
     ];
 ?>
 
@@ -29,6 +29,10 @@
         <h1> <?php echo $nome; ?> </h1>
         <p> <?php echo $bio; ?> </p>
         <div class="lista-links">
+     
+        
+        </div>
+        >
             <?php
                 foreach ($links as $texto => $url) {
                 $classe_extra = "";
@@ -41,7 +45,11 @@
                 $texto
                 </a>";
                 }
+
             ?>
+        </div>
+        <div class="area-contato">
+            <a href="contato.php" class="btn-msg">📧 Mandar Mensagem</a>
         </div>
     </div>
 </body>
