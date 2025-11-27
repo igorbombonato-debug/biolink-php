7da9b467a28b6f0643272f75031658579cea1701
igor Bombonato <igor.bombonato@aluno.senai.br>
2025-11-27 10:20:39 -0300

    criando loop com meus links pessoais
    é isso

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 
     <?php
     // --- DADOS DO PERFIL (PREENCHA COM SEUS DADOS) ---
-    $nome = "Charlies Brown jr";
-    $bio = "A vida me ensinou  nunca desistir";
+    $nome = "Charlies Brown jr♫♫";
+    $bio = "A vida me ensinou  nunca desistir<br>
+    @Santos<br>
+    𝕾𝖐𝖆𝖙𝖊 𝕭𝖔𝖆𝖗𝖉<br>";
     $imagem ="https://i.pinimg.com/736x/ee/f3/55/eef3556161bfa4884de825e6c16a0248.jpg"; // Pegue o link da SUA foto.
+    $links = [
+        "Instagram" => "https://instagram.com/iguinxit",
+        "WhatsApp"  => "https://wa.me/17981924356",
+        "Meu Jogo Favorito" => "https://play.google.com/store/apps/details?id=com.dts.freefireth&hl=pt_BR",
+        "Música do Momento" => "https://www.youtube.com/watch?v=oD9izvb3gO0"
+    ];
 ?>
 
 <!DOCTYPE html>
@@ -12,7 +20,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BioLink de <?php echo $nome; ?></title>
-
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="container">
@@ -20,8 +28,21 @@
 
         <h1> <?php echo $nome; ?> </h1>
         <p> <?php echo $bio; ?> </p>
-
-        <div class="lista-links"></div>
+        <div class="lista-links">
+            <?php
+                foreach ($links as $texto => $url) {
+                $classe_extra = "";
+                // DESAFIO: Se o texto for "WhatsApp" (ou outro que você queira), mude a variável $classe_extra para "destaque". Escreva o IF aqui:
+                if($texto == "Instagram"){
+                    $classe_extra="destaque";
+                }
+                // O PHP constrói o botão HTML
+                echo "<a href='$url' class='btn $classe_extra' target='_blank'>
+                $texto
+                </a>";
+                }
+            ?>
+        </div>
     </div>
 </body>
 </html>
